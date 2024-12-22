b6c4f4294bce602e37de49d771d4d10c5227fb28
Neil Thompson <github@neilthompson.co.uk>
2024-12-22 15:23:49 +0000

    More changes for full screen

--- src/index.php
+++ src/index.php
@@ -102,6 +102,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="apple-mobile-web-app-capable" content="yes">
+    <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
 
     <!-- Favicon -->
 	<link rel="shortcut icon" type="image/png" href="favicon.png">
